3aa6a469d32166b5d586aba4ed197f13eda53d75
Matt Barrington <mattbarrington7@gmail.com>
2023-07-05 18:48:39 -0400

    Cursor, left/right arrows keys

--- calculator.js
+++ calculator.js
@@ -1,48 +1,66 @@
-power=false;
-current="";
+//power starts as off
+let power=false;
+//current statement starts as an empty string
+let current="";
+//set of operator characters
 const operators = new Set(['+','-','*','/']);
+//constant for all buttons in the program
 const buttons = document.getElementsByTagName("button");
+//turn off all buttons since calculator is off
 for (const button of buttons) {
     button.disabled = true;
 }
+//on off button needs to be on at all times
 document.getElementById('onoff').disabled = false;
+//this calculator stores the previous two expressions and their answers
 memory = {
-    previousStatement:"0",
-    previousAnswer:"0",
-    precedingStatement:"0",
-    precedingAnswer:"0"
+    previousStatement:null,
+    previousAnswer:null,
+    precedingStatement:null,
+    precedingAnswer:null
+}
+const charWidth = 18;
+cursor = {
+    cursorID: document.getElementById('cursor'),
+    cursorOn: false,
+    cursorInterval: null,
+    cursorPos: 0,
+    cursorRow: 0
 }
 function togglePower(){
-    //This function 
+    //This function turns the power on or off
     power =! power;
     if(power) {
         document.getElementById('screen').style.backgroundColor = "white";
-        document.getElementById('displayOne').style.display = "block";
-        document.getElementById('displayTwo').style.display = "block";
+        document.getElementById('topRow').style.display = "block";
+        document.getElementById('bottomRow').style.display = "block";
         for (const button of buttons) {
             button.disabled = false;
         }
-        console.log('Power On...')
+        enableCursor();
     }
     else {
         document.getElementById('screen').style.backgroundColor = "grey";
-        document.getElementById('displayOne').style.display = "none";
-        document.getElementById('displayTwo').style.display = "none";
+        document.getElementById('topRow').style.display = "none";
+        document.getElementById('bottomRow').style.display = "none";
         clearScreen();
         for (const button of buttons) {
             button.disabled = true;
         }
-        console.log('Power Off...');
+        disableCursor();
     }
     document.getElementById('onoff').disabled = false;
 }
 function setTopRow(topDisplay){
-    document.getElementById('displayOne').innerHTML = topDisplay;
+    //sets the first row of the screen's display
+    document.getElementById('topRow').innerHTML = topDisplay;
 }
 function setBottomRow(bottomDisplay){
-    document.getElementById('displayTwo').innerHTML = bottomDisplay;
+    //sets the second row of the screen's display
+    document.getElementById('bottomDisplay').innerHTML = bottomDisplay;
 }
 function concatStatement(nextChar) {
+    //adds input to the current statement (operator first will take previous answer)
     if(current=="") {
         if(operators.has(nextChar)){
             concatStatement(memory.previousAnswer);
@@ -56,8 +74,11 @@ function concatStatement(nextChar) {
         current += nextChar;
     }
     setBottomRow(current);
+    moveCursor();
+    cursor.curserPos = current.length;
 }
-function readStatement(){//interprets a math problem
+function readStatement(){
+    //evaluates the current statement
     if(current==""){
         concatStatement(memory.previousAnswer);
         console.log("Previous answer fetched!");
@@ -68,6 +89,7 @@ function readStatement(){//interprets a math problem
     current="";
     setTopRow(ans);
     setBottomRow(current);
+    resetCursor();
 }
 function setMem(statement, answer){
     memory.precedingStatement=memory.previousStatement;
@@ -79,4 +101,49 @@ function clearScreen(){
     current="";
     setTopRow(current);
     setBottomRow(current);
+    resetCursor();
+}
+function moveCursor() {
+    cursor.cursorPos=current.length;
+    cursor.cursorID.style.left=cursor.cursorPos*charWidth + 'px';
+}
+function resetCursor(){
+    cursor.cursorPos = 0;
+    cursor.cursorID.style.left=0 + 'px';
+}
+function flipCursor(){
+    cursor.cursorOn = !(cursor.cursorOn);
+    if(cursor.cursorOn) {
+        cursor.cursorID.style.backgroundColor = "black";
+        cursor.cursorID.style.color = "black";
+    }
+    else {
+        cursor.cursorID.style.backgroundColor = "white";
+        cursor.cursorID.style.color = "white";
+    }
+}
+function enableCursor(){
+    clearInterval(cursor.cursorInterval);
+    cursor.cursorInterval = setInterval(flipCursor, 530);
+}
+function disableCursor(){
+    clearInterval(cursor.cursorInterval);
+}
+function moveLeft() {
+    if(cursor.cursorPos>0) {
+        cursor.cursorID.style.left = cursor.cursorPos*16.5 - 16.5 + 'px';
+        cursor.cursorPos--;
+    }
+}
+function moveRight() {
+    if(current.length>cursor.cursorPos) {
+        cursor.cursorID.style.left = cursor.cursorPos*16.5 + 16.5 + 'px';
+        cursor.cursorPos++;
+    }
+}
+function moveUp() {
+    if(cursor.cursorRow<4) {
+        cursor.cursorRow++;
+        
+    }
 }
